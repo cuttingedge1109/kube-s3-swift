97062a0b592200bcf203d79495958cf6747d95aa
cuttingedge1109 <53085803+cuttingedge1109@users.noreply.github.com>
2021-10-13 20:41:34 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -o errtrace
 IFS=$'\n\t'
 
 export S3_ACL=${S3_ACL:-private}
-
+UMASK=${UMASK:-0007}
 mkdir -p ${MNT_POINT}
 
 verbose=$(echo $VERBOSE | tr '[:upper:]' '[:lower:]')
@@ -16,8 +16,8 @@ echo "${AWS_KEY}:${AWS_SECRET_KEY}" > /etc/passwd-s3fs
 chmod 0400 /etc/passwd-s3fs
 
 if [ "$verbose" == "true" ]; then
-  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -d -d -f -o passwd_file=/etc/passwd-s3fs -o url=${S3_URL} -o use_path_request_style,allow_other,retries=5 -o umask=0007,uid=${MNT_UID},gid=${MNT_GID}
+  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -d -d -f -o passwd_file=/etc/passwd-s3fs -o url=${S3_URL} -o use_path_request_style,allow_other,retries=5 -o umask=${UMASK},uid=${MNT_UID},gid=${MNT_GID}
 else
-  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -f -o passwd_file=/etc/passwd-s3fs -o url=${S3_URL} -o use_path_request_style,allow_other,retries=5 -o umask=0007,uid=${MNT_UID},gid=${MNT_GID}
+  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -f -o passwd_file=/etc/passwd-s3fs -o url=${S3_URL} -o use_path_request_style,allow_other,retries=5 -o umask=${UMASK},uid=${MNT_UID},gid=${MNT_GID}
 fi
 echo 'started...'
